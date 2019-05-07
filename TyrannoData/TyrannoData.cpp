2485eefac4065f7b6de6c4b0dc9b26fbfe3689b5
caleb12-lee <caleb12.lee@gmail.com>
2019-05-08 04:41:44 +0900

    open av&subtitle files

--- TyrannoData/TyrannoData.cpp
+++ TyrannoData/TyrannoData.cpp
@@ -3,9 +3,35 @@
 
 #include "stdafx.h"
 
+extern "C" {
+#include "libavcodec\avcodec.h"
+#include "libavformat\avformat.h"
+}
 
-int main()
+// Audio & Video
+static AVFormatContext *fmt_ctx_av = NULL;
+static const char *src_filename_av = NULL;
+
+// Subtitle
+static AVFormatContext *fmt_ctx_sub = NULL;
+static const char *src_filename_sub = NULL;
+
+int main(int argc, char **argv)
 {
-    return 0;
+	src_filename_av = argv[1];
+	src_filename_sub = argv[2];
+
+	/* open input file, and allocate format context */
+	if (avformat_open_input(&fmt_ctx_av, src_filename_av, NULL, NULL) < 0) {
+		fprintf(stderr, "Could not open source file %s\n", src_filename_av);
+		exit(1);
+	}
+
+	if (avformat_open_input(&fmt_ctx_sub, src_filename_sub, NULL, NULL) < 0) {
+		fprintf(stderr, "Could not open source file %s\n", src_filename_sub);
+		exit(1);
+	}
+
+	return 0;
 }
 
